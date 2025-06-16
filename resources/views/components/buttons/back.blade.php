c872c765f8e9e577f75804ad785e7149b7a7e147
nekaru <32153003+ninajika@users.noreply.github.com>
2025-06-16 12:34:09 +0700

    Update back button to use ghost style and SVG icon

--- resources/views/components/buttons/back.blade.php
+++ resources/views/components/buttons/back.blade.php
@@ -1,4 +1,6 @@
-<button type="button" class="btn btn-secondary" onclick="history.back()">
-    <i class="fas fa-arrow-left me-2"></i>
+<button type="button" class="btn btn-ghost" onclick="history.back()">
+    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+    </svg>
     Back
 </button>
