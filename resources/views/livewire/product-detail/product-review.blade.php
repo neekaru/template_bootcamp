7f4d588381a5f77e52ca843672b82bc5f00aec7b
nekaru <32153003+ninajika@users.noreply.github.com>
2025-06-19 15:48:32 +0700

    refactor: Replace SVG rating stars with button elements for better accessibility

--- resources/views/livewire/product-detail/product-review.blade.php
+++ resources/views/livewire/product-detail/product-review.blade.php
@@ -24,11 +24,12 @@
             <div class="mb-6">
                 <div class="flex items-center space-x-1">
                     @for ($i = 1; $i <= 5; $i++)
-                        <svg wire:click="setRating({{ $i }})" wire:key="rating-star-{{ $i }}"
-                             class="w-10 h-10 cursor-pointer {{ $rating >= $i ? 'text-yellow-400' : 'text-gray-300 dark:text-gray-600' }}"
-                             fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                        </svg>
+                        <button type="button" wire:click="setRating({{ $i }})" wire:key="rating-star-{{ $i }}" class="p-0 m-0 bg-transparent border-none focus:outline-none">
+                            <svg class="w-10 h-10 cursor-pointer {{ $rating >= $i ? 'text-yellow-400' : 'text-gray-300 dark:text-gray-600' }}"
+                                 fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                            </svg>
+                        </button>
                     @endfor
                     <span class="ml-3 font-medium {{ $rating > 0 ? 'text-gray-700 dark:text-gray-300' : 'text-gray-500 dark:text-gray-400' }}">
                         {{ $rating > 0 ? $this->rating_text : 'Sangat baik?' }}
